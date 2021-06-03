a90443cd5452231d1358e01d290e1f34d316d4e4
Chris Bowen <info@cloud-wales.co.uk>
2021-06-03 06:43:32 +0100

    Got file transfer working. Need to work on refactoring

--- main.py
+++ main.py
@@ -4,55 +4,89 @@ import os
 import shutil
 from pathlib import Path
 
+
 # The path for listing items
 path = '/Users/chrisbowen/Downloads/'
+
+
+# To be implemented. Run through the dict
+file_types = {'.zip': 'Zip Files', '.jpg': 'Images', '.png': 'Images', '.iso': 'ISO Images', \
+                '.csv': 'CSV Files', '.dmg': 'Program Downloads', '.json': 'JSON Files', \
+                '.sql': 'SQL Files'}
 
 # The list of items
 files = os.listdir(path)
 
-def check_folder_exists(folder):
-    if Path(path + folder).is_dir():
+
+def check_folder_exists(folder_name):
+    if Path(path + folder_name).is_dir():
         return True
     else:
-        return False
+        os.makedirs(path + folder_name)
+        return True
+
+
+def move_files(folder_name, file_name):
+    if check_folder_exists(folder_name):
+        shutil.move(path + filename, path + folder_name)
 
 
 # Loop to print each filename separately
 for filename in files:
-    if ".zip" in filename:
-        if check_folder_exists("zip"):
-            shutil.move(path + filename, path + "zip/")
-        else:
-            os.makedirs(path + "zip/")
-    elif ".jpg" in filename:
-        if check_folder_exists("images"):
-            shutil.move(path + filename, path + "images/")
-        else:
-            os.makedirs(path + "images/")
-    elif ".iso" in filename:
-        if check_folder_exists("iso_images"):
-            shutil.move(path + filename, path + "iso_images/")
-        else:
-            os.makedirs(path + "iso_images/")
-    elif ".csv" in filename:
-        if check_folder_exists("csv_files"):
-            shutil.move(path + filename, path + "csv_files/")
-        else:
-            os.makedirs(path + "csv_files/")
-    elif ".dmg" in filename:
-        if check_folder_exists("programs"):
-            shutil.move(path + filename, path + "programs/")
-        else:
-            os.makedirs(path + "programs/")
-    elif ".json" in filename:
-        if check_folder_exists("json_files"):
-            shutil.move(path + filename, path + "json_files/")
-        else:
-            os.makedirs(path + "json_files/")
-    elif ".sql" in filename:
-        if check_folder_exists("sql_files"):
-            shutil.move(path + filename, path + "sql_files/")
-        else:
-            os.makedirs(path + "sql_files/")
 
+    if ".zip" in filename:
+        folder_name = "Zip Files"
+        move_files(folder_name, filename)
+
+    elif ".jpg" in filename:
+        folder_name = "Images"
+        move_files(folder_name, filename)
+
+    elif ".jpeg" in filename:
+        folder_name = "Images"
+        move_files(folder_name, filename)
+
+    elif ".png" in filename:
+        folder_name = "Images"
+        move_files(folder_name, filename)
+
+    elif ".gpx" in filename:
+        folder_name = "GPX Files"
+        move_files(folder_name, filename)
+
+    elif ".pdf" in filename:
+        folder_name = "PDF Files"
+        move_files(folder_name, filename)
+
+    elif ".iso" in filename:
+        folder_name = "ISO images"
+        move_files(folder_name, filename)
+
+    elif ".csv" in filename:
+        folder_name = "CSV Files"
+        move_files(folder_name, filename)
+
+    elif ".numbers" in filename:
+        folder_name = "Numbers Files"
+        move_files(folder_name, filename)
+
+    elif ".xml" in filename:
+        folder_name = "XML Files"
+        move_files(folder_name, filename)
+
+    elif ".dmg" in filename:
+        folder_name = "Program Downloads"
+        move_files(folder_name, filename)
+
+    elif ".json" in filename:
+        folder_name = "JSON Files"
+        move_files(folder_name, filename)
+
+    elif ".sql" in filename:
+        folder_name = "SQL Files"
+        move_files(folder_name, filename)
+
+    elif ".sqlite3" in filename:
+        folder_name = "SQL Files"
+        move_files(folder_name, filename)
 
